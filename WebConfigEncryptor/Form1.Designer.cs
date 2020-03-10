ced1c59ea7b81f1737b63fe20c603413a952c46e
Ted Krapf <tkrapf@townsquareenergy.com>
2020-03-09 21:55:30 -0500

    initial commit

---
+++ WebConfigEncryptor/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WebConfigEncryptor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetSites = new System.Windows.Forms.Button();
+            this.gvSites = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SiteName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Path = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.RunningState = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ConnectionStringState = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.AppSettingsState = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Action = new System.Windows.Forms.DataGridViewButtonColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gvSites)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGetSites
+            // 
+            this.btnGetSites.Location = new System.Drawing.Point(28, 12);
+            this.btnGetSites.Name = "btnGetSites";
+            this.btnGetSites.Size = new System.Drawing.Size(75, 23);
+            this.btnGetSites.TabIndex = 1;
+            this.btnGetSites.Text = "Get Sites";
+            this.btnGetSites.UseVisualStyleBackColor = true;
+            this.btnGetSites.Click += new System.EventHandler(this.btnGetSites_Click);
+            // 
+            // gvSites
+            // 
+            this.gvSites.AllowUserToAddRows = false;
+            this.gvSites.AllowUserToDeleteRows = false;
+            this.gvSites.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gvSites.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.SiteName,
+            this.Path,
+            this.RunningState,
+            this.ConnectionStringState,
+            this.AppSettingsState,
+            this.Action});
+            this.gvSites.Location = new System.Drawing.Point(28, 67);
+            this.gvSites.Name = "gvSites";
+            this.gvSites.ReadOnly = true;
+            this.gvSites.Size = new System.Drawing.Size(1233, 557);
+            this.gvSites.TabIndex = 2;
+            this.gvSites.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.gvSites_CellClick);
+            // 
+            // ID
+            // 
+            this.ID.DataPropertyName = "ID";
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            this.ID.ReadOnly = true;
+            // 
+            // SiteName
+            // 
+            this.SiteName.DataPropertyName = "SiteName";
+            this.SiteName.HeaderText = "SiteName";
+            this.SiteName.Name = "SiteName";
+            this.SiteName.ReadOnly = true;
+            // 
+            // Path
+            // 
+            this.Path.DataPropertyName = "Path";
+            this.Path.HeaderText = "Path";
+            this.Path.Name = "Path";
+            this.Path.ReadOnly = true;
+            // 
+            // RunningState
+            // 
+            this.RunningState.DataPropertyName = "RunningState";
+            this.RunningState.HeaderText = "RunningState";
+            this.RunningState.Name = "RunningState";
+            this.RunningState.ReadOnly = true;
+            // 
+            // ConnectionStringState
+            // 
+            this.ConnectionStringState.DataPropertyName = "ConnectionStringState";
+            this.ConnectionStringState.HeaderText = "ConnectionStringState";
+            this.ConnectionStringState.Name = "ConnectionStringState";
+            this.ConnectionStringState.ReadOnly = true;
+            // 
+            // AppSettingsState
+            // 
+            this.AppSettingsState.DataPropertyName = "AppSettingsState";
+            this.AppSettingsState.HeaderText = "AppSettingsState";
+            this.AppSettingsState.Name = "AppSettingsState";
+            this.AppSettingsState.ReadOnly = true;
+            // 
+            // Action
+            // 
+            this.Action.DataPropertyName = "Action";
+            this.Action.HeaderText = "Action";
+            this.Action.Name = "Action";
+            this.Action.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1305, 658);
+            this.Controls.Add(this.gvSites);
+            this.Controls.Add(this.btnGetSites);
+            this.Name = "Form1";
+            this.Text = "IIS Web.Config Encryptor";
+            ((System.ComponentModel.ISupportInitialize)(this.gvSites)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnGetSites;
+        private System.Windows.Forms.DataGridView gvSites;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SiteName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Path;
+        private System.Windows.Forms.DataGridViewTextBoxColumn RunningState;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ConnectionStringState;
+        private System.Windows.Forms.DataGridViewTextBoxColumn AppSettingsState;
+        private System.Windows.Forms.DataGridViewButtonColumn Action;
+    }
+}
+
---
+++ WebConfigEncryptor/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices;
+using Microsoft.Web.Administration;
+using System.Web;
+using System.Web.Configuration;
+using System.IO;
+
+namespace WebConfigEncryptor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class MySites
+        {
+            public long ID { get; set; }
+            public string SiteName { get; set; }
+            public string Path { get; set; }
+            public string RunningState { get; set; }
+            public string ConnectionStringState { get; set; }
+            public string AppSettingsState { get; set; }
+            public string Action { get; set; }
+
+            public MySites(long id, string name, string path, string state)
+            {
+                ID = id;
+                SiteName = name;
+                Path = path;
+                RunningState = state;
+                ConnectionStringState = ConfigStatus(path, "connectionStrings");
+                AppSettingsState = ConfigStatus(path, "appSettings"); ;
+                Action = ConnectionStringState == "Protected" ? "decrypt me" : "encrypt me";
+            }
+        }
+
+        private void btnGetSites_Click(object sender, EventArgs e)
+        {
+            MySiteGrid_Refresh();
+        }
+
+        public void MySiteGrid_Refresh()
+        {
+            List<MySites> mySites = new List<MySites>();
+            ServerManager iis = new ServerManager();
+            foreach(Site site in iis.Sites)
+                mySites.Add(new MySites(site.Id, site.Name, site.Applications["/"].VirtualDirectories[0].PhysicalPath, site.State.ToString()));
+            mySites = mySites.OrderBy(c => c.SiteName).ToList();
+
+            gvSites.RowHeadersVisible = false;
+            gvSites.DataSource = mySites;
+
+            foreach(DataGridViewColumn c in gvSites.Columns)
+            {
+                int width = c.GetPreferredWidth(DataGridViewAutoSizeColumnMode.AllCells, false);
+                c.Width = width;
+            }
+        }
+
+        private void mySitesBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gvSites_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string colName = gvSites.CurrentCell.OwningColumn.Name;
+
+            string path = gvSites.CurrentRow.Cells["Path"].Value.ToString();
+            int siteID = Convert.ToInt32(gvSites.CurrentRow.Cells["ID"].Value.ToString());
+            string action = gvSites.CurrentRow.Cells["Action"].Value.ToString();
+
+            string connectionStringState = gvSites.CurrentRow.Cells["ConnectionStringState"].Value.ToString();
+            string appSettingState = gvSites.CurrentRow.Cells["AppSettingsState"].Value.ToString();
+
+            switch (colName)
+            {
+                case "Action":
+                    if (action.ToLower() == "encrypt me")
+                    {
+                        if (connectionStringState != "not present") EncryptSite(siteID, path, section_ConnStr);
+                        if (appSettingState != "not present")       EncryptSite(siteID, path, section_AppSettings);
+                    }
+                    else if (action.ToLower() == "decrypt me")
+                    {
+                        if (connectionStringState != "not present") DecryptSite(siteID, path, section_ConnStr);
+                        if (appSettingState != "not present")       DecryptSite(siteID, path, section_AppSettings);
+                    }
+                    MySiteGrid_Refresh();
+                    break;
+            }
+        }
+
+
+        #region de/encrypt
+
+        string provider = "RSAProtectedConfigurationProvider";
+        string section_ConnStr = "connectionStrings";
+        string section_AppSettings = "appSettings";
+
+        public static string ConfigStatus(string path, string configType)
+        {
+            var config = OpenConfigFile(path + "\\web.config");
+            var section = config.GetSection(configType);
+
+            if (section == null)
+            {
+                return "unknown, configSection null";
+            }
+            else if (section != null && section.ElementInformation.IsPresent)
+            {
+                bool isProtected = section.SectionInformation.IsProtected;
+                if (isProtected)
+                    return "Protected";
+                else
+                    return "Unprotected";
+            }
+            else
+                return "not present";
+        }
+
+        private void EncryptSite(int siteID, string path, string sectionType)
+        {
+            var config = OpenConfigFile(path + "\\web.config");
+            var section = config.GetSection(sectionType);
+
+            if (section != null)
+            {
+                bool isProtected = section.SectionInformation.IsProtected;
+                if (isProtected)
+                {
+                    MessageBox.Show("This is already encrypted, you cannot do this.");
+                    return;
+                }
+                section.SectionInformation.ProtectSection(provider);
+                config.Save();
+                MessageBox.Show("Config section ("+sectionType+") ENCRYPTED!");
+            }
+            else
+            {
+                MessageBox.Show("This config section is null, not encrypted.");
+                return;
+            }
+        }
+        private void DecryptSite(int siteID, string path, string sectionType)
+        {
+            var config = OpenConfigFile(path + "\\web.config");
+            var section = config.GetSection(sectionType);
+
+            if (section != null)
+            {
+                bool isProtected = section.SectionInformation.IsProtected;
+                if (!isProtected)
+                {
+                    MessageBox.Show("This is already decrypted, you cannot do this.");
+                    return;
+                }
+                section.SectionInformation.UnprotectSection();
+                config.Save();
+                MessageBox.Show("Config section (" + sectionType + ") DECRYPTED!");
+            }
+            else
+            {
+                MessageBox.Show("This config section is null, not encrypted.");
+                return;
+            }
+        }
+
+
+        public static System.Configuration.Configuration OpenConfigFile(string configPath)
+        {
+            var configFile = new FileInfo(configPath);
+            var vdm = new VirtualDirectoryMapping(configFile.DirectoryName, true, configFile.Name);
+            var wcfm = new WebConfigurationFileMap();
+            wcfm.VirtualDirectories.Add("/", vdm);
+            return System.Web.Configuration.WebConfigurationManager.OpenMappedWebConfiguration(wcfm, "/");
+        }
+
+        #endregion
+    }
+}
